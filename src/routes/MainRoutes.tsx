4fe6b28210b3e71a1686879ecdfcffd4eae602e5
giangleee <giangle602@gmail.com>
2023-06-20 23:08:22 +0700

    Build request info

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -12,6 +12,7 @@ const Sample = Loadable(lazy(() => import("../views/SampleView")));
 const NewRequestView = Loadable(lazy(() => import("../views/NewRequestView")));
 const RequestList = Loadable(lazy(() => import("../views/RequestListView")));
 const RequestListWithoutAccept = Loadable(lazy(() => import("../views/RequestListWithoutAccept")));
+const RequestInfo = Loadable(lazy(() => import('../views/RequestInfo/RequestInfo')));
 
 const MainRoutes: Array<MainRoutesInterface> = [
   { path: "/", component: Sample },
@@ -24,6 +25,7 @@ const MainRoutes: Array<MainRoutesInterface> = [
   { path: "/request/new", component: NewRequestView },
   { path: "/request-list-without-accept", component: RequestListWithoutAccept },
   { path: "/request-list", component: RequestList },
+  { path: "/request/id", component: RequestInfo},
 ];
 
 export default MainRoutes;
---
+++ src/views/RequestInfo/RequestInfo.tsx
@@ -0,0 +1,200 @@
+import "./style.scss";
+import { FC } from "react";
+import { Button, FormControl, Grid, TextField, Typography } from "@mui/material";
+
+const RequestInfo: FC = () => {
+  return (
+    <div className="new-request__container">
+      <h4>リクエスト詳細</h4>
+      <div className="detail-container">
+        <FormControl
+          fullWidth
+          className="flex flex-row justify-between form-control__name"
+        >
+          <Grid
+            item
+            className="w-full"
+          >
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              スタッフ
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              multiline
+              rows={1}
+              defaultValue="Default Value"
+            />
+          </Grid>
+          <Grid
+            item
+            className="w-full"
+          >
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              スターテス
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              multiline
+              rows={1}
+              defaultValue="Default Value"
+            />
+          </Grid>
+        </FormControl>
+      </div>
+      <div className="detail-container">
+        <FormControl fullWidth>
+          <Grid item>
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              仕事の詳細
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              multiline
+              rows={3}
+              defaultValue="Default Value"
+            />
+          </Grid>
+        </FormControl>
+      </div>
+      {/* <SelectTimeComponent /> */}
+      <div className="detail-container pt-2">
+        <FormControl
+          fullWidth
+          className="flex flex-row justify-between form-control__name"
+        >
+          <Grid
+            item
+            className="w-full"
+          >
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              時間
+            </Typography>
+            <div className="flex items-center">
+              <Grid
+                item
+                className="flex items-center justify-between"
+              >
+                <Typography
+                  variant="h5"
+                  component="h5"
+                  sx={{ padding: "5px" }}
+                  className="typo-custom"
+                >
+                  から
+                </Typography>
+                <TextField
+                  fullWidth
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={1}
+                  defaultValue="Default Value"
+                  className="w-8/12"
+                />
+              </Grid>
+              <Grid
+                item
+                className="flex items-center justify-between"
+              >
+                <Typography
+                  variant="h5"
+                  component="h5"
+                  sx={{ padding: "5px" }}
+                  className="typo-custom"
+                >
+                  まで
+                </Typography>
+                <TextField
+                  fullWidth
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={1}
+                  defaultValue="Default Value"
+                  className="w-8/12"
+                />
+              </Grid>
+            </div>
+          </Grid>
+          <Grid
+            item
+            className="w-full"
+          >
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              給料
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              multiline
+              rows={1}
+              defaultValue="Default Value"
+            />
+          </Grid>
+        </FormControl>
+      </div>
+      <div className="detail-container">
+        <FormControl fullWidth>
+          <Grid item>
+            <Typography
+              variant="h5"
+              component="h5"
+              sx={{ padding: "5px" }}
+              className="typo-custom"
+            >
+              その他の注意事項
+            </Typography>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              defaultValue="Default Value"
+              rows={2}
+            />
+          </Grid>
+        </FormControl>
+      </div>
+      <div className="flex justify-evenly button__container">
+        <Button
+          variant="contained"
+          className="button"
+        >
+          レビュー
+        </Button>
+        <Button
+          variant="contained"
+          className="button"
+        >
+          バック
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default RequestInfo;
