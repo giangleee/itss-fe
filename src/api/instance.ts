b6a0c874dcad7137f7c7b26ffc586a7d09f05497
sonbk040901 <leducson007@gmail.com>
2023-07-04 02:24:48 +0700

    fix some bug

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,18 +1,19 @@
 import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from "axios";
 import { toast } from "react-toastify";
 const instance = axios.create({
-  baseURL: "https://itss-be-production.up.railway.app/api/v1",
+  baseURL: "http://localhost:3000/api/v1",
 });
 
 const onRequest = (config: InternalAxiosRequestConfig) => {
   config.headers.set("Content-Type", "application/json");
   const token = localStorage.getItem("token");
-  config.headers.Authorization = token ? "Bearer " + localStorage.getItem("token") : undefined;
+  const BearerToken = token ? "Bearer " + token : undefined;
+  config.headers.Authorization = BearerToken
   return config;
 };
 const onResponse = (response: AxiosResponse) => {
   if (response.config.url !== "/auth/login") return response;
-  const token = response.data.data;
+  const token = response.data.data.token;
   if (token) {
     localStorage.setItem("token", token);
   }
